616ba549dfd03a55579e5186580ed063e605b24a
odiks938 <odiks938@gmail.com>
2026-06-24 12:02:27 +0700

    update

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import user from "../controllers/user.controller.js";
+import {register, login} from "../controllers/user.controller.js";
 
 const router = express.Router();
+
 router.post("/", register);
 router.post("/login", login);
 
